475b0750264b864ecd88da162136e4d12a1ff398
monstercj <261345723@qq.com>
2025-06-14 12:54:26 +0800

    - 修改菜单名字展示

--- config/modules/03_base_software_installation/00_base_software_installation_menu.sh
+++ config/modules/03_base_software_installation/00_base_software_installation_menu.sh
@@ -87,7 +87,7 @@ declare -A BASE_SOFTWARE_INSTALLATION_MENU_ENTRIES=(
     # 使用 'core_modules' 键指向 MODULES_DIR (即 config/modules)
     [1]="基础命令行软件 (fd tar ncdu reflector ...)|action:core_modules:03_base_software_installation/01_install_essential_software.sh"
     [2]="输入法框架及方案 (Fcitx5-im  fcitx5-chinese-addons)|action:core_modules:03_base_software_installation/02_install_input_method.sh"
-    [3]="常用软件安装 ( 多列表可供选择 )|action:core_modules:03_base_software_installation/03_install_software_Allinone.sh"
+    [3]="软件安装ALL-IN-ONE ( 多列表可供选择 )|action:core_modules:03_base_software_installation/03_install_software_Allinone.sh"
     [4]="xxxx (Essential, Common, Specific Apps)|menu:core_modules:04_software_installation/00_software_installation_menu.sh"
     [5]="XXXX|action:core_modules:00_cleanup_and_finish.sh"
 
